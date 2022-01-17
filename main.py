82b4ca82220f93927d528b45d1e4f03b73a5e658
N44 <91179000+n694420@users.noreply.github.com>
2022-01-17 20:11:16 +0530

    Added the code

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
-#Stack Implementations!
+# Stack Implementations!
+# Push Stack
+def push(stack):
+    item=int(input("enter the item"))
+    stack.append(item)
+    top=len(stack)-1
+#  Pop Stack
+def pop(stack):
+    if (stack)==[]:
+        print('underflow')
+    else:
+     x=stack.pop()
+     print('poped element is',x)
+     if len(stack)==0:
+              top=None
+     else:
+      top=len(stack)-1
+# Peek(Top) Stack     
+def peek(stack):
+    if stack==[]:
+       print('underflow')
+    else:
+     top=len(stack)-1
+     print('peek is',stack[top])
+# Display Stack    
+def display(stack):
+    if stack==[]:
+       print('underflow')
+    else:
+        top=len[stack]-1
+       for x in range(top-1,-1):
+       print(stack[x])
+      
+stack=[]
+top=None
+print('Stack Operations!\n1 Push\n2 Pop\n3 Peek\n4 Display \n5 Exit\n')
+While True:
+
+
+  ch=int(input('enter your choice'))
+  if ch==1:
+       push(stack)
+  elif ch==2:
+       pop(stack)
+  elif ch==3:
+       peek(stack)
+  elif ch==4:
+       display(stack)
+  elif ch==5:
+          break;
+  else:
+     print('Please provide a valid choice!')
